b56cb60e5b408c70aa1c0710fbbec36619a8c4a7
hurmf37 <148119784+hurmf37@users.noreply.github.com>
2024-09-30 17:43:17 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,101 @@
+token = '6435555856:AAE5z5Yq8rxVy6MJLml3h_wnd1BpQ2cP2uk'
+
+syntax = {
+    'gastronomy': {
+        'name': 'Гастрономия',
+        'coffee_shops': {
+            'name': 'Кофейни',
+            'fortochka': 'Форточка',
+            'keks': 'Кекс',
+            'suft_coffee': 'Suft Coffee',
+            'wknd_center': 'Wknd Center'
+        },
+        'bars': {
+            'name': 'Бары',
+            'adventure': 'Авантюра',
+            'beginning': 'Начало',
+            'hollywood': 'Hollywood',
+            'innocence': 'Невинность',
+            'shamaika_house': 'Шамайка House'
+        },
+        'restaurants': {
+            'name': 'Рестораны',
+            'crayfish_and_reptiles': 'Раки и гады',
+            'drink_coffee': 'Пить кофе',
+            'matador': 'Матадор',
+            'moon_dead': 'Луна дохлая',
+            'old_man_khinkalich': 'Шамайка House'
+        }
+    },
+    'entertainments': {
+        'name': 'Развлечения',
+        'animals': {
+            'name': 'Животные',
+            'dolphinarium': 'Дельфинарий',
+            'ostrich_farmstead': 'Страусиное подворье',
+            'touching_zoo': 'Трогательный зоопарк',
+            'zoo': 'Зоопарк'
+        },
+        'clubs': {
+            'name': 'Клубы',
+            'endgi_show': 'Endgi Show',
+            'hypnosis': 'Гипноз',
+            'paramonov': 'ПарамоновЪ',
+            'punch': 'Пунш'
+        },
+        'movie_theaters': {
+            'name': 'Кинотеатры',
+            'anti_cinema': 'Своя атмосфера',
+            'city_cinema_roof': 'Кино крыша | City Cinema Roof',
+            'horizon': 'Горизонт',
+            'kinomax': 'Киномакс'
+        }
+    },
+    'cultural': {
+        'name': 'Культура',
+        'exhibitions': {
+            'name': 'Выставки',
+            'art_bazar': 'Арт Базар',
+            'through_your_eyes': 'Твоими глазами'
+        },
+        'museums': {
+            'name': 'Музеи',
+            'art_gallery_pushkin': 'Арт-галерея на Пушкинской',
+            'communication_development': 'Музей истории развития связи на Дону',
+            'don_library': 'Выставочный зал Донской государственной публичной библиотеки',
+            'gallery_nnn': 'Музей-галерея Nnn',
+            'ncr': 'Музей железнодорожной техники под открытым небом',
+            'russian_armenian_friendship': 'Музей русско-армянской дружбы',
+            'vintage_clocks': 'Музей Старинных часов'
+        }
+    },
+    'public_spaces': {
+        'name': 'Публичные пространства',
+        'anti_cafe': {
+            'name': 'Анти кафе',
+            'paws_and_tails': 'Лапы и хвосты'
+        },
+        'embankments': {
+            'name': 'Набережные',
+            'embankment_rostov': 'Набережная Ростова-на-Дону'
+        },
+        'parks': {
+            'name': 'Парки',
+            'alley_roses': 'Аллея Роз',
+            'botanical_garden': 'Ботанический сад',
+            'donstu': 'Студенческий парк ДГТУ',
+            'first_mile': 'Первая Миля',
+            'grove_ska': 'Роща СКА',
+            'levoberezhny': 'Левобережный',
+            'maksim_gorky': 'Центральный парк им. М. Горького',
+            'north_square': 'Северный сквер',
+            'revolution': 'Парк Революции'
+        },
+        'viewing_platforms': {
+            'name': 'Смотровые площадки',
+            'voroshilovsky_bridge': 'Смотровая площадка у Ворошиловского моста',
+            'zmievskaya_balka': 'Смотровая площадка у Змиёвской балки'
+        }
+    }
+    }
+extensions = ['png', 'jpg', 'jpeg', 'heic']
